1027324c5c837ecddcd3e0251989e0ade4961678
Ana Avalos <annyavalos8@gmail.com>
2024-07-26 21:49:55 -0500

    Config de webpack e inicio de estructura html

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,33 @@
 const path = require('path');
 
-module.exports= {
-entry: './src/index.js',// punto de entrada de tu aplicacion
-output:{
-filename: 'bundle.js', // nombre del archivo de salida
-path: path.resolve(_dirname,'dist'),//carpeta de salida
-},
-module:{
-    rules:[
-{
-    test: /\.css$/, // Regex para identificar archivos CSS
-    use: ['style-loader','css-loader']
-},
-{
-    test: /\.js$/,
-    exclude: /node_modules/,
-    use:{
-        loader: 'babel-loader',
-        options:{
-          presets:  ['@babel/preset-env'] ,    
+module.exports={
+    entry:'./src/index.js', 
+    output:{
+        filename: 'bundle.js',
+path:path.resolve(dirname,'dist'),
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+use: ['style-loader','css-loader'],
+            },
+            {
+                test:/\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader:'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    }
+                }
+            }
+        ]
+    },
+    devtool: 'source-map',
+    devServer: {
+        contentBase: path.resolve(_dirname,'dist'),
+        compress: true,
+        port: 9000,
     }
-}
-}
-] 
-},
-devtool: 'source-map',
-devServer: {
-    contentBase: path.resolve(_dirname,'dist'),
-    compress: true,
-    port: 9000,
-}
 }
